98813ed3e82a9b79e87ed6e2ebc2f5dd5bca12b1
Granit <ghaxhaj@fordham.edu>
2020-02-19 17:28:22 -0500

    hey

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", function(event) {
+    const form = document.getElementById('create-task-form')
+    const description = document.getElementById("new-task-description")
+    const toDo = document.getElementById("tasks")
+
+    form.addEventListener("submit", function(event){
+    event.preventDefault();
+    const newTask = document.createElement("li")
+    const newDescription = form.newTaskDescription.value
+    newTask.innerText = newDescription
+    toDo.append(newTask)
+    const button = document.createElement("button")
+    newTask.append(button)
+    button.innerText = "Delete"
+    button.addEventListener("click", function(event){
+      event.target.parentNode.remove()
+    })
+
+    })
+})
